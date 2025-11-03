b4bc14c667d39ce858fa9da1e9f6bcae4f51f2dc
Simrat-07 <simrat.pal07@gmail.com>
2025-11-03 12:56:37 +0530

    endpoint add

--- app.py
+++ app.py
@@ -1,8 +1,48 @@
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return "hello lode"
+db  = SQLAlchemy(app)
+api = Api(app)
+CORS(app)
+
+
+app.config['SECRET_KEY']='SUPER-SECRET-KEY'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///databse.db'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), unique = True, nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+
+with app.app_context():
+    db.create_all()
+
+class UserRegisteration(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        if not username or not password:
+            return {'message':'Missing username or password'}, 400
+        if User.query.filter_by(username = username).first():
+            return {'message':'Username already taken'}, 400
+        
+        new_user = User(username=username,password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return {'message':'User created successfully'}, 200
+    
+    
+
+
+
+
+
+
+
+
 if __name__ in  '__main__':
     app.run(debug=True)
