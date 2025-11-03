96a195b6340ed9e1d34b5d8caaef9dfc89e3763e
rajviir13 <rajvirsingh3978@gmail.com>
2025-11-03 16:07:22 +0530

    connected

---
+++ static/signup.js
@@ -0,0 +1,18 @@
+async function signup() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    const response = await fetch("http://localhost:5000/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+    alert(data.message);
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Signup failed.");
+  }
+}
